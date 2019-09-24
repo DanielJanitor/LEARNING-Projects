4a0ca0ad0945635f1271bfe2a3d19993c7963436
DanielJanitor <janitor.daniel@gmail.com>
2019-09-24 21:15:28 +0200

    update

--- Game_Rock_Paper_Scissors/main.js
+++ Game_Rock_Paper_Scissors/main.js
@@ -1,7 +1,7 @@
 const message = document.querySelector('.message');
 const score = document.querySelector('.score');
 const btns = document.querySelectorAll('button');
-
+let winner = [0, 0];
 for (let i = 0; i < btns.length; i++) {
     btns[i].addEventListener('click', game);
 
@@ -23,14 +23,29 @@ function game(button) {
     let result = checkWinner(playerSelection, computerSelection);
     console.log(result);
 
+    if (result == 'Player') {
+        result += 'Wins';
+        winner[0]++;
+    } else if (result == 'Computer') {
+        result += 'Wins';
+        winner[1]++;
+    } else {
+        result += 'Result in tie Match'
+    }
+    score.innerHTML = 'Player[' + winner[0] + '] vs Computer [' + winner[1] + ']';
+    messanger(playerSelection + ' vs ' + computerSelection + '<br><b>' + result + '<b>');
+}
 
-
+function messanger(mes) {
+    message.innerHTML = mes;
 }
 
 function checkWinner(pl, cp) {
     if (pl === cp) {
-        return 'Draw';
+        return 'Same Chosse! ';
     }
+
+
     if (pl === 'Rock') {
         if (cp === 'Paper') {
             return 'Computer';
@@ -40,6 +55,13 @@ function checkWinner(pl, cp) {
     }
     if (pl === 'Paper') {
         if (cp === 'Scissiors') {
+            return 'Computer';
+        } else {
+            return 'Player';
+        }
+    }
+    if (pl === "Scissors") {
+        if (cp === "Rock") {
             return 'Computer';
         } else {
             return 'Player';
