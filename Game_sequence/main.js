31571f1504fe79d539225cb22528431f3cfc0601
DanielJanitor <janitor.daniel@gmail.com>
2019-10-05 20:11:34 +0200

    update

--- Game_sequence/main.js
+++ Game_sequence/main.js
@@ -1,12 +1,12 @@
 let myBlock;
 let myFunctionList;
-let funList;
+let funList = [];
 document.addEventListener('DOMContentLoaded', function () {
     myBlock = document.createElement('div');
     myBlock.textContent = 'hello world';
     document.body.appendChild(myBlock);
-    
-
+    myFunctionList = document.createElement('div');
+    document.body.appendChild(myFunctionList);
 
     myBlock.style.width = '100px';
     myBlock.style.height = '100px';
@@ -25,19 +25,42 @@ document.addEventListener('keydown', function (e) {
     console.log(keyC);
     if (keyC === 38) {
         addFun('top');
+        goTop();
     } else if (keyC === 40) {
         addFun('down');
+        goDown();
     } else if (keyC === 39) {
         addFun('right');
+        goRight();
     } else if (keyC === 37) {
         addFun('left');
+        goLeft();
     } else if (keyC === 32) {
         myBlock.style.backgroundColor = randomColor();
     }
 })
 
+function mover() {
+    if (funList.length > 0) {
+        let
+    }
+}
+
 function addFun(val) {
-     
+    funList.push(val);
+    console.log(funList);
+    let span = document.createElement('span');
+    span.textContent = '+ ' + val;
+    span.style.padding = '10px';
+    span.style.border = '1px solid black';
+    myFunctionList.appendChild(span);
+    span.addEventListener('mouseover', () => {
+        span.style.backgroundColor = 'green';
+    })
+
+    span.addEventListener('mouseout', () => {
+        span.style.backgroundColor = 'white';
+    })
 }
 
 
