73c125244124444fef909df98ba58a706d8c7af3
DanielJanitor <janitor.daniel@gmail.com>
2019-10-05 20:33:52 +0200

    update

---
+++ Game_Magic_B/main.js
@@ -0,0 +1,13 @@
+const message = document.querySelector('.message');
+const question = document.querySelector('input');
+const btn = document.querySelector('button');
+const answerArray = ['it will work','Maybe maybe not', 'Higly likely', 'I do not know'];
+const answer = document.querySelector('.answer');
+
+
+btn.addEventListener('click', ()=>{
+    let res = Math.floor(Math.random()*answerArray.length);
+    let output = question.value + ', ' + answerArray[res];
+    answer.innerHTML = output;
+    question.value = '';
+})
