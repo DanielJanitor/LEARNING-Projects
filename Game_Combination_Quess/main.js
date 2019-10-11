6e7c9fe7896e2b89747354dfa5d27b6bb7f537b6
DanielJanitor <janitor.daniel@gmail.com>
2019-10-11 21:42:26 +0200

    u[date

--- Game_Combination_Quess/main.js
+++ Game_Combination_Quess/main.js
@@ -1,33 +1,57 @@
 const gameAre = document.querySelector('.game');
-const btn = document.querySelector('button');
-let gamePlay = false;
-btn.addEventListener('click', ()=>{
-    console.log(gamePlay);
-    
-    if (gamePlay === false) {
-        gamePlay = true;
-        btn.innerHTML = 'Check Combo';
-        maker();
-    }else{
-        console.log('Check');
-        
+const button = document.querySelector('button');
+const message = document.querySelector('.message');
+let playGame = false;
+let score = 0;
+button.addEventListener('click', function () {
+    if (!playGame) {
+        playGame = true;
+        score = 0;
+        gameAre.innerHTML = '';
+        maker(6);
+        button.innerHTML = 'Check Combo';
+        message.innerHTML = 'Guess the combo';
+    } else {
+        let winContition = 0;
+        score++;
+        message.innerHTML = 'Guesses ' + score;
+        const numbers = document.querySelectorAll('.numb');
+        for (let i = 0; i < numbers.length; i++) {
+            if (numbers[i].correct == numbers[i].value) {
+                numbers[i].style.backgroundColor = 'green';
+                numbers[i].style.color = 'white';
+                winContition++;
+            } else {
+                let color = (numbers[i].value < numbers[i].correct) ? 'blue' : 'red';
+                numbers[i].style.backgroundColor = color;
+                numbers[i].style.color = 'black';
+            }
+            if (winContition == numbers.length) {
+                gameEnd();
+            }
+        }
     }
 })
 
-function maker(){
-   for (let x = 0; x < 4; x++) {
-    
-    
-    let el = document.createElement('input');
-    el.setAttribute('type','number');
-    el.max = 9;
-    el.min = 0;
-    el.order = x;
-    el.size = 1;
-    el.value = 0;
-    el.correct = Math.random();
-    el.classList.add('numb');
-    el.style.width = '40px';
-    gameAre.appendChild(el);
-    };
-};
+function gameEnd() {
+    message.innerHTML = 'You solved combo in ' + score + ' gueses.';
+    playGame = false;
+    score = 0;
+    button.innerHTML = 'Restart';
+}
+
+function maker(num) {
+    for (let i = 0; i < num; i++) {
+        let el = document.createElement('input');
+        gameAre.appendChild(el);
+        el.setAttribute('type', 'number');
+        el.classList.add('numb');
+        el.max = 9;
+        el.min = 0;
+        el.order = i;
+        el.size = 1;
+        el.correct = Math.floor(Math.random() * 10);
+        el.value = 0;
+        el.style.width = '50px';
+    }
+}
