26ab68a63a4cb378838871052532294dacf0ca80
Daniel Janitor <55194125+DanielJanitor@users.noreply.github.com>
2019-10-14 15:55:47 +0200

    Add files via upload

---
+++ Click_Image_Code/main.js
@@ -0,0 +1,20 @@
+const images = document.querySelectorAll('.pop img');
+const output = document.querySelector('.output');
+const show = document.querySelector('.show');
+const close = document.querySelectorAll('.close');
+images.forEach(function(ele){
+  console.log(ele);
+  ele.addEventListener('click', popImage);
+})
+
+close.forEach(function(ele){
+  ele.addEventListener('click',function(){
+    show.classList.add('hide');
+  })
+})
+
+function popImage(e){
+  console.log(this.src);
+  output.querySelector('img').setAttribute('src',this.src);
+  show.classList.remove("hide");
+}
